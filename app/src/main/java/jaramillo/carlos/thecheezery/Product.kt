36f7b5d698f162787758bf597a905bbc77fdf27b
Daniel <danielmillo7@hotmail.com>
2021-09-23 00:19:18 -0700

    Menus

---
+++ app/src/main/java/jaramillo/carlos/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package jaramillo.carlos.thecheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
